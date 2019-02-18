abf8f127d00e056f33d1b5293bf733a3c15d05df
Krzysztof Bober <kbober@zendesk.com>
2019-02-18 22:34:17 +0100

    syntax, values, operators and other simple stuff

--- exercises/values/conversion_exrc.js
+++ exercises/values/conversion_exrc.js
@@ -7,7 +7,7 @@ Instructions:
 import { strict as assert } from 'assert';
 
 test('conversion', () => {
-  assert.equal(Boolean(5), '???');
-  assert.equal(Number('7'), '???');
-  assert.equal(String(21), '???');
+  assert.equal(Boolean(5), true);
+  assert.equal(Number('7'), 7);
+  assert.equal(String(21), '21');
 });
--- exercises/variables-assignment/const_exrc.js
+++ exercises/variables-assignment/const_exrc.js
@@ -10,6 +10,7 @@ test('const', () => {
   const x = 3;
   assert.equal(x, 3);
   {
+    const x = 12;
     assert.equal(x, 12);
   }
   assert.equal(x, 3);
